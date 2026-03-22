978eaf325389671f116ffa679ec561953dba4bbb
Puspita Das <puspitadas869@gmail.com>
2026-03-22 20:35:08 +0530

    Update app and views

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -11,8 +12,29 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/read', (req, res) => {
-    res.render('read');
+app.get('/read', async(req, res) => {
+    const users = await userModel.find();
+    res.render('read', { users});
+});
+
+app.get('/delete/:id', async(req, res) => {
+    const users = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect('/read');
+});
+
+app.post('/create', async (req, res) => {
+    let { name, email, image } = req.body;
+
+    if (!name || !email || !image) {
+        return res.status(400).send('All fields are required.');
+    }
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image,
+    });
+    res.redirect('/read');
 });
 
 app.listen(3000);
